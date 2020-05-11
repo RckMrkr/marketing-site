f3781c6a6aa962321423b749a6f63be58015f0f1
Rick <rick@rickmarker.dk>
2020-05-11 15:20:09 +0200

    eloomi integrations almost done

--- src/components/contactForm/contactForm.js
+++ src/components/contactForm/contactForm.js
@@ -13,29 +13,34 @@ export default ({from, isDark}) => {
     name, setName,
     email, setEmail,
     message, setMessage,
+    showModal, setShowModal,
+    isSubmitting, setIsSubmitting,
   } = useContactForm();
 
-  const handleSubmit = async (e) => {
-    alert('Yo');
-    try {
-      console.log(encode({ "form-name": "contact", name, email, message, sentFrom: from }))
-      // await fetch("/", {
-      //   method: "POST",
-      //   headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      //   body: encode({ "form-name": "contact", name, email, message, sentFrom: from })
-      // });
-      alert('Success');
-    } catch(error){
-      alert(error)
-    }
 
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    try {
+      setIsSubmitting(true)
+      await new Promise(resolve => setTimeout(resolve, 5000));
+      await fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({ "form-name": "contact", name, email, message, sentFrom: from })
+      });
+      setShowModal('success');
+      setIsSubmitting(false);
+    } catch(error){
+      setIsSubmitting(false);
+      setShowModal('error');
+    }
   }
   const setProperty = (event, setter) => setter(event.target.value);
   return (
     <section className={classname("section", {"is-dark": isDark})}>
       <div className="container">
-        <h4 class="title has-text-centered">How may we help?</h4>
+        <h4 className="title has-text-centered">How may we help?</h4>
         <div className="columns is-horizontal-center">
           <div className="column is-two-thirds">
             <form onSubmit={handleSubmit}>
@@ -44,7 +49,7 @@ export default ({from, isDark}) => {
                   <div className="field">
                     <label className="label" htmlFor='contactFormName'>Name</label>
                     <div className="control">
-                      <input id="contactFormName" onInput={e => setProperty(e, setName)}  value={name} className="input" type="text" />
+                      <input id="contactFormName" onInput={e => setProperty(e, setName)} value={name} className="input" type="text" />
                     </div>
                   </div>
                 </div>
@@ -57,8 +62,8 @@ export default ({from, isDark}) => {
                   </div>
                 </div>
               </div>
-              <div class="columns">
-              <div class="column">
+              <div className="columns">
+              <div className="column">
                   <div className="field">
                     <label className="label" htmlFor='contactFormMessage'>Message</label>
                     <div className="control">
@@ -70,7 +75,7 @@ export default ({from, isDark}) => {
                 <div className="column">
                   <div className="field is-flex is-horizontal-center">
                     <p className="control">
-                      <button type="submit" className="button is-success">
+                      <button type="submit" className={classname("button", "is-success", {"is-loading": isSubmitting, 'disabled': isSubmitting})}>
                         Send
                       </button>
                     </p>
@@ -80,6 +85,30 @@ export default ({from, isDark}) => {
             </form>
           </div>
         </div>
+      </div>
+      <div className={classname('modal', {'is-active': showModal === 'success'})}>
+        <div className="modal-background" role="button" onClick={() => setShowModal(null)}></div>
+        <div className="modal-content">
+          <div className="box is-success">
+            <h5 className="title has-text-white">Thank you</h5>
+            <p className="has-text-white">Your message has been submitted and we will get back to you shortly, usually in less than 1 business day.</p>
+          </div>
+        </div>
+        <button className="modal-close is-large" aria-label="close"></button>
+      </div>
+      <div className={classname('modal', {'is-active': showModal === 'error'})}>
+        <div className="modal-background" role="button" onClick={() => setShowModal(null)}></div>
+        <div className="modal-content">
+          <div className="box is-danger">
+            <h5 className="title">We're so sorry!</h5>
+            <p>
+              An error occured during the submission, so it is possible the submission didn't go thought.<br />
+              Please reach out using our email <a className="has-text-macaroon" href="mailto:contact@interops.io">contact@interops.io</a> instead.<br /><br />
+              And, again, we are very sorry for the inconvenience.
+            </p>
+          </div>
+        </div>
+        <button className="modal-close is-large" aria-label="close"></button>
       </div>
     </section>
   )
--- src/components/contactForm/hooks/useContactForm.js
+++ src/components/contactForm/hooks/useContactForm.js
@@ -1,14 +1,32 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
+const EscapeKeys = ['27', 'Escape'];
 export default () => {
   
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [showModal, setShowModal] = useState(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  useEffect(() => {
+    const handler = ({key}) => {
+      if (EscapeKeys.includes(String(key))) {
+        setShowModal(null);
+      }
+    };
+    window.addEventListener('keyup', handler);
+
+    return () => {
+      window.removeEventListener('keyup', handler);
+    };
+  }, []);
 
   return {
     name, setName,
     email, setEmail,
     message, setMessage,
+    showModal, setShowModal,
+    isSubmitting, setIsSubmitting,
   };
 }
